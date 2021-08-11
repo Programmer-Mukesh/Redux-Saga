a9a87919e68b58468286d4e83179d9c4e51462f0
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-08-11 16:04:45 +0530

    Update index.js

--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -1,35 +1,37 @@
 import {
   GET_USERS_REQUESTED,
   GET_USERS_SUCCESS,
-  GET_USERS_FAILED
-} from  "../Action/userTypes";
+  GET_USERS_FAILED,
+} from "../Action/userTypes";
 
 const initialState = {
   users: [],
   loading: false,
   error: null,
-}
+};
 
-export default function users(state = initialState, action) {
+const users = (state = initialState, action) => {
   switch (action.type) {
     case GET_USERS_REQUESTED:
       return {
         ...state,
         loading: true,
-      }
+      };
     case GET_USERS_SUCCESS:
       return {
         ...state,
         loading: false,
-        users: action.users
-      }
+        users: action.users,
+      };
     case GET_USERS_FAILED:
       return {
         ...state,
         loading: false,
         error: action.message,
-      }
+      };
     default:
       return state;
   }
-}
+};
+
+export default users;
